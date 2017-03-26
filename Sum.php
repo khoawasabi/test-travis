05b6b79114e4b575785915b5da88b49fa08713aa
khoawasabi <khoawasabi@gmail.com>
2017-03-26 16:46:19 +0900

    first commit

---
+++ Sum.php
@@ -0,0 +1,8 @@
+<?php
+
+class Sum {
+	public static function getSum($number_1, $number_2)
+	{
+		return $number_1 + $number_2;
+	}
+}
---
+++ SumTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class SumTest extends PHPUnit_Framework_TestCase {
+	/**
+	@test
+	*/
+	public function testSum()
+	{
+		$this->assertEquals(3, 3);
+	}
+}
+?>
