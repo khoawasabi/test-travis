d0d13f4608fbcea62e0149e0e0d5d3e5b7996268
khoawasabi <khoawasabi@gmail.com>
2017-04-03 16:21:53 +0900

    asdfjaksdf fucker2

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-echo "testing"
+echo "testing";
 // ensure we get report on all possible php errors
 error_reporting(E_ALL); // same meaning as error_reporting(-1)
 // Beside, we could use error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
